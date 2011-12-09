1beed0c2f851fead32ab9717f45885b0feaa6dd6
Joaquín Rosales <globojorro@gmail.com>
2011-12-09 11:11:43 -0600

    Built using 'paster create -t plone_app collective.formwidget.relationfield; using same dependencies as plone.formwidget.contenttree

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.formwidget.relationfield',
+      version=version,
+      description="Provides a widget for adding related items using configurable catalog queries",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Plone :: 4.1",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Joaquin Rosales',
+      author_email='globojorro@gmail.com',
+      url='https://github.com/collective/collective.formwidget.relationfield',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.formwidget'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'z3c.formwidget.query',
+          'plone.formwidget.autocomplete >= 1.2.0',
+          'plone.z3cform >= 0.7.4',
+      ],
+      extras_require={
+        'test': ['plone.app.testing'],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
