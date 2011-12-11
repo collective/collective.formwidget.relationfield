5c173bbab5d82cd031542745d3a7b67d4e098a36
Joaquín Rosales <globojorro@gmail.com>
2011-12-11 15:11:40 -0600

    Added i18n support for package

--- collective/formwidget/relationfield/__init__.py
+++ collective/formwidget/relationfield/__init__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
 
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory('collective.formwidget.relationfield')
