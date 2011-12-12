1cfaa65cdab9f3ca2d3a4b55edb0f12f1247322e
Joaquín Rosales <globojorro@gmail.com>
2011-12-11 22:24:38 -0600

    Added render_tree method to widget which allows args: relPath,query,limit

--- collective/formwidget/relationfield/widget.py
+++ collective/formwidget/relationfield/widget.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from zope.component import getMultiAdapter
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
 from zope.interface import implementer
 from zope.i18n import translate
 
+from plone.app.layout.navigation.interfaces import INavtreeStrategy
+from plone.app.layout.navigation.navtree import buildFolderTree
 from plone.formwidget.contenttree.widget import MultiContentTreeWidget
 import z3c.form.interfaces
 import z3c.form.widget
@@ -16,6 +19,28 @@ class ContentRelationWidget(MultiContentTreeWidget):
     display_template = ViewPageTemplateFile('related_display.pt')
     input_template = ViewPageTemplateFile('related_input.pt')
     recurse_template = ViewPageTemplateFile('related_recurse.pt')
+
+    def render_tree(self, relPath=None, query=None, limit=10):
+        content = self.context
+        portal_state = getMultiAdapter((self.context, self.request),
+                                          name=u'plone_portal_state')
+        portal = portal_state.portal()
+        source = self.bound_source
+        if query is not None:
+            source.navigation_tree_query = query
+        strategy = getMultiAdapter((portal, self), INavtreeStrategy)
+        if relPath is not None:
+            root_path = portal_state.navigation_root_path()
+            rel_path = root_path + '/' + relPath
+            strategy.rootPath = rel_path
+        data = buildFolderTree(portal,
+                               obj=portal,
+                               query=source.navigation_tree_query,
+                               strategy=strategy)
+
+        return self.recurse_template(
+                                    children=data.get('children', [])[:limit],
+                                    level=1)
 
     def js_extra(self):
         form_url = self.request.getURL()
